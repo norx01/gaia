6e6f9bdaa60d1c6c141e72e4cdc5543289149e61
norx <juan_pinillos@hotmail.com>
2026-05-15 15:03:07 -0500

    web

--- src/main/java/gaia/gaia/view/UsuariosView.java
+++ src/main/java/gaia/gaia/view/UsuariosView.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UsuariosView
@@ -27,4 +31,29 @@ public class UsuariosView
         model.addAttribute("usuarios", new Usuarios());
         return "usuarios/form";
     }
+
+    @PostMapping("/view/usuarios/save")
+    public String save(@ModelAttribute Usuarios usuarios, RedirectAttributes ra)
+    {
+        usuariosRepository.save(usuarios);
+        ra.addFlashAttribute("mensaje", "Usuario guardado con exito");
+        return "redirect:/view/usuarios";
+    }
+
+    @GetMapping("/view/usuarios/edit/{id}")
+    public String edit(@PathVariable Long id, Model model)
+    {
+        Usuarios usuarios = usuariosRepository.findById(id).orElse(null);
+        model.addAttribute("usuarios", usuarios);
+        return "usuarios/form";
+    }
+
+    @PostMapping("/view/usuarios/delete/{id}")
+    public String delete(@PathVariable Long id, RedirectAttributes ra)
+    {
+        usuariosRepository.deleteById(id);
+        ra.addFlashAttribute("mensaje", "Usuario eliminado con exito");
+        return "redirect:/view/usuarios";
+    }
+
 }
